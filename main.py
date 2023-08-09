919fc4b204cfbd1a9ff300094f730a30e2a537d0
bculo <culobozo@gmail.com>
2023-08-09 23:31:01 +0200

    Database setup for sqlite added

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 from fastapi import FastAPI, status
+from starlette.middleware.cors import CORSMiddleware
 from starlette.requests import Request
 from starlette.responses import JSONResponse
 from domain.exceptions import CoreException, NotFoundException
 from routers import users, items
+from sql import models
+from sql.database import engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(users.router)
 app.include_router(items.router)
-
 
 @app.exception_handler(CoreException)
 async def unicorn_exception_handler(request: Request, exc: CoreException):
--- routers/users.py
+++ routers/users.py
@@ -1,4 +1,13 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, Query
+from sqlalchemy.orm import Session
+
+from domain.exceptions import NotFoundException, CoreException
+
+from sql import crud, models, schemas
+from sql.database import engine, SessionLocal
+from sql.dependencies import get_db
 
 router = APIRouter(
     prefix="/users",
@@ -6,9 +15,23 @@ router = APIRouter(
 )
 
 
-@router.get("/")
-async def read_users():
-    return [{"username": "Rick"}, {"username": "Morty"}]
+@router.post("/create/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise CoreException("Email already registered")
+    return crud.create_user(db=db, user=user)
+
+
+@router.get("/page")
+async def user_page(take: Annotated[int, Query()], skip: Annotated[int, Query()], db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip, take)
+    if len(users) == 0:
+        return []
+    models = []
+    for user in users:
+        models.append(schemas.User(**user.__dict__))
+    return models
 
 
 @router.get("/me")
@@ -19,3 +42,4 @@ async def read_user_me():
 @router.get("/{username}")
 async def read_user(username: str):
     return {"username": username}
+
